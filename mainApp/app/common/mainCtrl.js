ada3476dc9be0ee0a71ed2f48c6260c8aaa29858
kiran paturi <kpaturi@smarttuition.com>
2016-07-20 15:16:08 +0530

    added code

---
+++ mainApp/app/common/mainCtrl.js
@@ -0,0 +1,6 @@
+angular.module("bestBuy")
+    .controller("mainCtrl", function () {
+        this.headerUrl = "app/common/header.html";
+        this.footerUrl = "app/common/footer.html";
+        this.contentUrl = "";
+    });
