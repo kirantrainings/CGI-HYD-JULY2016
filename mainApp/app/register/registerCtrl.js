36acc064e6365c1c9b17f02a7372bba43ca08f17
kiran paturi <kpaturi@smarttuition.com>
2016-07-20 15:02:15 +0530

    added code for second half

--- mainApp/app/register/registerCtrl.js
+++ mainApp/app/register/registerCtrl.js
@@ -1,7 +1,14 @@
 angular.module("register")
     .controller("registerCtrl", function () {
         this.user = {};
-
+        this.countries = [{
+                name: "India",
+                code: "IN"
+            },
+            {
+                name: "Canada",
+                code: "CA"
+            }];
         this.registerUser = function () {
             //send data to the api.
             console.log(this.user);
