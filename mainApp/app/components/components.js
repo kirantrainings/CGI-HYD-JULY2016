089103a51e35079d72e4e1b486dc2cde32c2cb06
kiran paturi <kpaturi@smarttuition.com>
2016-07-21 13:04:55 +0530

    added code for the directives

--- mainApp/app/components/components.js
+++ mainApp/app/components/components.js
@@ -16,3 +16,22 @@ angular.module("components")
             restrict: "AEC"
         };
     });
+
+angular.module("components")
+    .directive("alphabetsOnly", function () {
+        return {
+            restrict: "A",
+            link: function (scope, element, attrs) {
+                function alphabets(evt) {
+                    if ((evt.keyCode >= 97 && evt.keyCode <= 122) ||
+                        (evt.keyCode >= 65 && evt.keyCode <= 90) ||
+                        evt.keyCode == 32) {
+
+                    } else {
+                        evt.preventDefault();
+                    }
+                }
+                element.bind("keypress", alphabets);
+            }
+        }
+    });
